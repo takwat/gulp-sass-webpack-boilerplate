b3bd6d1d9bee558ff2fef04bafaae32ba797f420
TAK <takwat@users.noreply.github.com>
2018-08-17 19:37:40 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const csscomb = require('gulp-csscomb');
+const grouping = require('gulp-group-css-media-queries');
+
+const webpackStream = require('webpack-stream');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+
+gulp.task('sass', () => {
+	return gulp.src('sass/**/*.scss')
+	.pipe(plumber({
+		errorHandler: function(err) {
+			console.error(err);
+		}
+	}))
+	.pipe(sass())
+	// .pipe(sourcemaps.write())
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(grouping())
+	.pipe(csscomb())
+	.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('sass:watch', () => {
+	return gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('webpack', () => {
+	return webpackStream(webpackConfig, webpack).pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('webpack:watch', () => {
+	return gulp.watch(['js/**/*.js', 'js/**/*.vue'], ['webpack']);
+});
+
+
+gulp.task('default', ['sass', 'webpack', 'sass:watch', 'webpack:watch']);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+	mode: 'development',
+	// mode: 'production',
+	entry: path.resolve(__dirname, 'src', 'app.js'),
+	output: {
+		filename: 'app.js'
+	},
+	resolve: {
+		modules: [path.join(__dirname, 'src'), 'node_modules'],
+		extensions: ['.js', '.vue'],
+		alias: {
+		  'vue$': 'vue/dist/vue.esm.js' // for es module
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+	]
+}
